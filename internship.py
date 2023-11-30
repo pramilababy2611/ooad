dff88551b7ed62d74d1352f0e537a4fe5c708c33
pramilababy2611 <115171792+pramilababy2611@users.noreply.github.com>
2023-11-30 14:46:56 +0530

    Add files via upload

---
+++ internship.py
@@ -0,0 +1,40 @@
+# coding=System
+from REGISTER import *
+from DEPARTMNT import *
+
+class PROJECT (REGISTER):
+
+  """
+   
+
+  :version:
+  :author:
+  """
+
+  """ ATTRIBUTES
+
+   
+
+  NUMER  (private)
+
+   
+
+  TOPIC  (private)
+
+   
+
+  DEPARMENT  (private)
+
+  """
+
+  def VIEW(self):
+    """
+     
+
+    @return  :
+    @author
+    """
+    pass
+
+
+
